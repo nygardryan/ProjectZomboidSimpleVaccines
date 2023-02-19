28eab2a83d9301e384e19ab709347e93e41678d5
nygardryan <nygardryan@yahoo.com>
2023-02-18 19:18:57 -0700

    Added Moodle Updates and fixed many bugs

--- First Aid Vaccines/media/lua/client/FAVACCINEMoodle.lua
+++ First Aid Vaccines/media/lua/client/FAVACCINEMoodle.lua
@@ -8,48 +8,39 @@ isMoodleFrameWorkEnabled = getActivatedMods():contains("MoodleFramework");
 vaccine_increment_amt = 0.0001
 
 function FAVVaccineMoodlePlayers()
-    print("THIS!!")
-    local players = getOnlinePlayers();
-
-    if players
-    then
-        for i = 0, players:size()-1 do
-            FAVVaccineMoodleFunction(players:get(i));
-        end
-    else
-        FAVVaccineMoodleFunction(getSpecificPlayer(0))
-    end
+    FAVUtils.getSingleVsOnlinePlayer(FAVVaccineMoodleFunction)
 end
 
 function FAVVaccineMoodleFunction(player)
-    InitializeTable(player)
+    FAVUtils.InitializeTable(player)
 
     mod_data = player:getModData()
 
     print("***************************")
     print(mod_data.vac_increasing)
-
+    print(mod_data.current_vaccine_level)
     print(mod_data.vaccine_power)
 
     print(isMoodleFrameWorkEnabled)
     print("HERE")
+    print("******************************")
 
     if isMoodleFrameWorkEnabled
     then
         if mod_data.current_vaccine_level > (mod_data.vaccine_power * 0.7)
         then
-            MF.getMoodle("vaccine_moodle"):setValue(0.8);   
+            MF.getMoodle("vaccine_moodle"):setValue(0.6);   
             if mod_data.vac_increasing == 1
-                then
+            then
                     MF.getMoodle("vaccine_moodle"):setChevronIsUp(true);
-                else
+            else
                     MF.getMoodle("vaccine_moodle"):setChevronIsUp(false);
-                end
+            end
             
 
-        elseif mod_data.current_vaccine_level =< 0
+        elseif mod_data.current_vaccine_level <= 0
         then
-            MF.getMoodle("vaccine_moodle"):setValue(0.2)
+            MF.getMoodle("vaccine_moodle"):setValue(0.4)
             MF.getMoodle("vaccine_moodle"):setChevronIsUp(false);
         else
             MF.getMoodle("vaccine_moodle"):setValue(0.5)
