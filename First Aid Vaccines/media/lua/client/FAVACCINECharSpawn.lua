fefe9cd59f7e464bbd08dd9fb88b93f39ce8480c
nygardryan <nygardryan@yahoo.com>
2023-03-28 17:44:41 -0600

    Description updates

--- First Aid Vaccines/media/lua/client/FAVACCINECharSpawn.lua
+++ First Aid Vaccines/media/lua/client/FAVACCINECharSpawn.lua
@@ -3,7 +3,6 @@ local MOD_NAME = "Improved First Aid + Vaccines";
 local MOD_VERSION = '1.0';
 local MOD_AUTHOR = "BlueBerry Gravy";
 local MOD_DESCRIPTION = "Adds easier ways to level first aid skill and craftable vaccines to help prevent zombie infections"
-local debugItems = TRUE;
 
 
 local function info()
